52069932fb6d521642feff549e8e87160d488071
CubeStar1 <143164352+CubeStar1@users.noreply.github.com>
2025-03-09 15:14:56 +0530

    fix: updated imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import { Hero } from "@/components/landing/hero";
-import { About } from "@/components/landing/about";
-import { Venue } from "@/components/landing/venue";
+import { Hero } from "@/components/landing/hero-section";
+import { About } from "@/components/landing/about-section";
+import { Venue } from "@/components/landing/venue-section";
 import Timeline from "@/components/landing/timeline";
 import Gallery from "@/components/landing/gallery";
 
